9f0b880379ea3626427f281babe0379b4d7c0d95
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2024-11-14 16:59:41 +0100

    sa

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,16 +1,23 @@
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
+import siteConfig from '../data/site-config';
 
 export const GET: APIRoute = async ({ site }) => {
     const blog = await getCollection('blog');
     const pages = await getCollection('pages');
+    const staticPaths = [
+        ...siteConfig.headerNavLinks?.map(link => link.href) || [],
+        ...siteConfig.footerNavLinks?.filter(link => !link.href.startsWith('http')).map(link => link.href) || []
+    ];
 
     const xmlString = `<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            <url>
-                <loc>${site?.origin}</loc>
-                <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
-            </url>
+            ${staticPaths.map(path => `
+                <url>
+                    <loc>${site?.origin}${path}</loc>
+                    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+                </url>
+            `).join('')}
             ${blog.map(post => `
                 <url>
                     <loc>${site?.origin}/blog/${post.slug}</loc>
