efec90d53b3fe4efe3b4273439019cf3f30a3d35
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2024-11-14 16:54:46 +0100

    xz

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -2,37 +2,33 @@ import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 
 export const GET: APIRoute = async ({ site }) => {
-   // Recupera tutte le collezioni
-   const blog = await getCollection('blog');
-   const pages = await getCollection('pages');
-   // Aggiungi altre collezioni se necessario
+    const blog = await getCollection('blog');
+    const pages = await getCollection('pages');
 
-   return new Response(
-       `<?xml version="1.0" encoding="UTF-8"?>
-       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-           <url>
-               <loc>${site?.origin}/</loc>
-               <priority>1.0</priority>
-           </url>
-           ${blog.map(post => `
-               <url>
-                   <loc>${site?.origin}/blog/${post.slug}/</loc>
-                   <lastmod>${post.data.publishDate.toISOString()}</lastmod>
-                   <priority>0.7</priority>
-               </url>
-           `).join('')}
-           ${pages.map(page => `
-               <url>
-                   <loc>${site?.origin}/${page.slug}/</loc>
-                   <lastmod>${page.data.lastUpdated?.toISOString()}</lastmod>
-                   <priority>0.8</priority>
-               </url>
-           `).join('')}
-       </urlset>`,
-       {
-           headers: {
-               'Content-Type': 'application/xml'
-           }
-       }
-   );
+    const xmlString = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <url>
+                <loc>${site?.origin}</loc>
+                <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+            </url>
+            ${blog.map(post => `
+                <url>
+                    <loc>${site?.origin}/blog/${post.slug}</loc>
+                    <lastmod>${post.data.lastUpdated?.toISOString().split('T')[0] || post.data.publishDate?.toISOString().split('T')[0]}</loc>
+                </url>
+            `).join('')}
+            ${pages.map(page => `
+                <url>
+                    <loc>${site?.origin}/${page.slug}</loc>
+                    <lastmod>${page.data.lastUpdated?.toISOString().split('T')[0]}</loc>
+                </url>
+            `).join('')}
+        </urlset>`.trim();
+
+    return new Response(xmlString, {
+        headers: {
+            'Content-Type': 'application/xml',
+            'Cache-Control': 'max-age=3600'
+        }
+    });
 }
