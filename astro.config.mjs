e0607557439478fe6d26290c8fc5edaca492cece
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2024-12-01 18:06:19 +0100

    ghjk

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   site: 'https://www.serp-secrets.com',
   output: 'server',
   adapter: netlify(),
-  trailingSlash: 'ignore',
+  trailingSlash: 'always',
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
